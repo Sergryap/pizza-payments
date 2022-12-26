6918e67786e9846ecabe5a4741b241c05c4b2c79
Sergryap <rs1180@mail.ru>
2022-12-26 21:52:11 +0500

    The create_product and create_pcm_product functions has been added to api-store.py module

--- api-store.py
+++ api-store.py
@@ -1,8 +1,11 @@
 import os
 import time
+from pprint import pprint
+
 import requests
 
 from environs import Env
+from slugify import slugify
 
 
 def check_token(error=False):
@@ -22,8 +25,72 @@ def check_token(error=False):
         os.environ['ACCESS_TOKEN'] = token_data['access_token']
 
 
+def create_product(name, sku, description, price):
+    url = 'https://api.moltin.com/v2/products'
+    headers = {
+        'Authorization': f'Bearer {os.environ["ACCESS_TOKEN"]}',
+        'Content-Type': 'application/json'
+    }
+    json_data = {
+        'data': {
+            'type': 'product',
+            'name': name,
+            'slug': slugify(name),
+            'sku': sku,
+            'manage_stock': False,
+            'description': description,
+            'price': [
+                {
+                    'amount': price,
+                    'currency': 'RUB',
+                    'includes_tax': True
+                }
+            ],
+            'status': 'live',
+            'commodity_type': 'physical'
+        }
+    }
+    response = requests.post(url=url, headers=headers, json=json_data)
+    response.raise_for_status()
+    return response.json()
+
+
+def create_pcm_product(name, sku, description):
+    url = 'https://api.moltin.com/pcm/products'
+    headers = {
+        'Authorization': f'Bearer {os.environ["ACCESS_TOKEN"]}',
+        'Content-Type': 'application/json'
+    }
+    json_data = {
+        'data': {
+            'type': 'product',
+            'attributes': {
+                'name': name,
+                'commodity_type': 'physical',
+                'sku': sku,
+                'slug': slugify(name),
+                'description': description,
+                'status': 'live',
+
+            },
+        }
+    }
+    response = requests.post(url=url, headers=headers, json=json_data)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_pcm_products():
+    url = 'https://api.moltin.com/pcm/products'
+    headers = {'Authorization': f'Bearer {os.environ["ACCESS_TOKEN"]}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
 if __name__ == '__main__':
     env = Env()
     env.read_env()
     check_token()
-    print(os.environ['ACCESS_TOKEN'])
+    pprint(create_pcm_product('test test test2', 'test-6', 'description-test'))
+    # pprint(get_pcm_products())
