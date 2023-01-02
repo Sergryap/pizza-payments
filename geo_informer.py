ed40738707fa757b1721027fe0e4ba6880d05167
sergryap <rs1180@mail.ru>
2023-01-02 22:21:51 +0500

    The get_min_distance_branch function has been added to geo_informer module

--- geo_informer.py
+++ geo_informer.py
@@ -1,4 +1,6 @@
 import requests
+from geopy import distance
+from api_store import get_all_entries
 
 
 def fetch_coordinates(apikey, address):
@@ -18,3 +20,21 @@ def fetch_coordinates(apikey, address):
     lng, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
 
     return float(lat), float(lng)
+
+
+def get_min_distance_branch(client_pos):
+    if client_pos:
+        branches = get_all_entries()
+        branch_distances = []
+        for branch in branches['data']:
+            branch_pos = (branch['latitude'], branch['longitude'])
+            dist = distance.distance(client_pos, branch_pos).km
+            branch_distances.append(
+                {'address': branch['address'], 'dist': dist}
+            )
+        branch_distances.sort(key=lambda x: x['dist'])
+        selected_branch = branch_distances[0]
+
+        return selected_branch['address'], selected_branch['dist']
+    return None, None
+
